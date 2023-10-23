0409c3e2fbd18f911d75d27c86766cf8c4d4f4e5
澪 <mio@mio19.uk>
2023-10-23 15:01:01 +0800

    initial Bonus class

---
+++ src/main/java/pdc/project/Bonus.java
@@ -0,0 +1,15 @@
+package pdc.project;
+
+import java.awt.*;
+import java.awt.image.ImageObserver;
+
+public class Bonus extends AbstractEntity implements Drawable {
+    public Bonus(double x, double y) {
+        super(x, y, 10, 10);
+    }
+
+    @Override
+    public void draw(Graphics2D g2d, ImageObserver obs) {
+        // TODO
+    }
+}
