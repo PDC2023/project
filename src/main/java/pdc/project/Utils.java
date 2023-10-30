df184831dc1f566f8f18e1652410aa4f182382d7
澪 <mio@mio19.uk>
2023-10-30 11:42:17 +0800

    define Utils.scaleImageByRatio

--- src/main/java/pdc/project/Utils.java
+++ src/main/java/pdc/project/Utils.java
@@ -8,10 +8,18 @@ public final class Utils {
     public static Image loadImage(String path) {
         return new ImageIcon(Objects.requireNonNull(Utils.class.getResource(path))).getImage();
     }
+
     public static Image scaleImage(Image image, int width, int height) {
         Image scaledImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
         return scaledImage;
     }
+
+    public static Image scaleImageByRatio(Image image, float ratio) {
+        int newWidth = (int) (image.getWidth(null) * ratio);
+        int newHeight = (int) (image.getHeight(null) * ratio);
+        return scaleImage(image, newWidth, newHeight);
+    }
+
 
     public static void drawImage(Graphics2D g2d, Image image, int x, int y) {
         g2d.drawImage(image, (int) x - image.getWidth(null) / 2, (int) y - image.getHeight(null) / 2, null);
