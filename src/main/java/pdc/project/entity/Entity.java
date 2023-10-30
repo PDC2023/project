9d73dd7fac57fdb3f92ec3de7ab1873919d14a68
澪 <mio@mio19.uk>
2023-10-30 14:49:20 +0800

    implement Entity.getCollision()

--- src/main/java/pdc/project/entity/Entity.java
+++ src/main/java/pdc/project/entity/Entity.java
@@ -13,6 +13,7 @@ public interface Entity extends Drawable {
     int getY();
 
     CollisionBox getCollisionBox();
+
     default boolean checkCollision(Entity other) {
         int otherX = other.getX();
         int otherY = other.getY();
@@ -28,6 +29,47 @@ public interface Entity extends Drawable {
         int deltaY = Math.abs(this.getY() - otherY);
 
         return deltaX <= (thisHalfWidth + otherHalfWidth) && deltaY <= (thisHalfHeight + otherHalfHeight);
+    }
+
+    default CollisionInfo getCollision(Entity other) {
+        int otherX = other.getX();
+        int otherY = other.getY();
+        CollisionBox otherBox = other.getCollisionBox();
+        CollisionBox thisBox = this.getCollisionBox();
+
+        int thisHalfWidth = thisBox.getWidth() / 2;
+        int thisHalfHeight = thisBox.getHeight() / 2;
+        int otherHalfWidth = otherBox.getWidth() / 2;
+        int otherHalfHeight = otherBox.getHeight() / 2;
+
+        int deltaX = Math.abs(this.getX() - otherX);
+        int deltaY = Math.abs(this.getY() - otherY);
+
+        CollisionState state = CollisionState.NONE;
+        CollisionDirection direction = CollisionDirection.NONE;
+
+        boolean isTouchingX = deltaX == (thisHalfWidth + otherHalfWidth);
+        boolean isTouchingY = deltaY == (thisHalfHeight + otherHalfHeight);
+        boolean isOverlappingX = deltaX < (thisHalfWidth + otherHalfWidth);
+        boolean isOverlappingY = deltaY < (thisHalfHeight + otherHalfHeight);
+
+        boolean isCollisionX = isTouchingX || isOverlappingX;
+        boolean isCollisionY = isTouchingY || isOverlappingY;
+
+        if (isCollisionX && isCollisionY) {
+            int overlappingByX = (thisHalfWidth + otherHalfWidth) - deltaX;
+            int overlappingByY = (thisHalfHeight + otherHalfHeight) - deltaY;
+            if (overlappingByX > overlappingByY) {
+                direction = (this.getY() < otherY) ? CollisionDirection.DOWN : CollisionDirection.UP;
+                state = isTouchingX ? CollisionState.TOUCHING : CollisionState.OVERLAPPING;
+            } else {
+                direction = (this.getX() < otherX) ? CollisionDirection.RIGHT : CollisionDirection.LEFT;
+                state = isTouchingY ? CollisionState.TOUCHING : CollisionState.OVERLAPPING;
+            }
+            return new CollisionInfo(state, direction);
+        } else {
+            return new CollisionInfo(CollisionState.NONE, CollisionDirection.NONE);
+        }
     }
 
     default void tick() {
