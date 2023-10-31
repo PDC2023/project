11008f94c5df04b18c41c9fa7f990ac4b126b7fd
澪 <mio@mio19.uk>
2023-10-31 14:04:22 +0800

    bigger background

--- src/main/java/pdc/project/entity/BackgroundEntity.java
+++ src/main/java/pdc/project/entity/BackgroundEntity.java
@@ -7,6 +7,6 @@ import java.awt.*;
 
 public class BackgroundEntity extends ImageEntity implements NoSpaceEntity {
     public BackgroundEntity(Universe universe, int x, int y) {
-        super(universe, x, y, 0, 0, Utils.loadImage("/background.png"));
+        super(universe, x, y, 0, 0, Utils.loadImage("/background.png", 2));
     }
 }
