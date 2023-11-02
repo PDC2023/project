4a7304e41fa3b299be5034deb33d94459ad775ec
澪 <mio@mio19.uk>
2023-11-02 10:06:08 +0800

    Revert "get ghost size from the image"
    This reverts commit ebd443cc2a08a7c3486cc77ef9c3e3d5ec3098fc.

--- src/main/java/pdc/project/entity/Ghost.java
+++ src/main/java/pdc/project/entity/Ghost.java
@@ -12,7 +12,7 @@ public class Ghost extends AbstractMovingEntity implements Enemy {
     private final int xStart;
 
     public Ghost(int xStart, int xEnd, Universe universe, int y) {
-        super(universe, xStart, y, Utils.loadImage("/ghost.png", SIZE_RATIO));
+        super(universe, xStart, y, (int) (32 * SIZE_RATIO), (int) (32 * SIZE_RATIO), Utils.loadImage("/ghost.png", SIZE_RATIO));
         imageFacingRight = false;
         horizontalVelocity = SPEED;
         this.xStart = xStart;
